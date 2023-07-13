df111faee8183628caf6a1a56fa237d6609cc0b0
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-13 12:52:50 +0300

    Update the app to get started

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import {ReactElement} from 'react';
 import './App.css';
 
-function App() {
+const App: () => ReactElement | null = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className='App'>
+            <h1>Weather App</h1>
         </div>
     );
 }
