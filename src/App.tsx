729859c6c74464c41e8097d401effdb2ad17faba
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-13 14:19:28 +0300

    Add Search to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import React, {ReactElement, useState} from 'react';
+import {ReactElement} from 'react';
 import './App.css';
 import Header from './components/Header';
+import Search from './components/Search';
 
 const App: () => ReactElement | null = () => {
-    const [city, setCity] = useState('');
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCity(event.target.value);
-    };
-
-    const handleSearch = (): void => {
-        console.log(city);
-    };
-
     return (
         <>
             <Header/>
-            <div className='container'>
-                <input
-                    className='input'
-                    type="text"
-                    value={city}
-                    onChange={handleInputChange}
-                />
-                <button
-                    className='button'
-                    onClick={handleSearch}
-                >
-                    Add
-                </button>
-            </div>
+            <Search/>
         </>
     );
 }
