553e1fe42d6c54c3cdd637dec6171f5f88d91ebf
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-13 20:15:27 +0300

    Center the search field

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -13,31 +13,32 @@ const Search: React.FC<SearchInterface> = (
     }
 ) => {
     return (
-        <div className='container'>
-            <input
-                className='input'
-                type='text'
-                value={city}
-                onChange={onInputChange}
-            />
-            <ul className='list'>
-                {locations.map((location: LocationInterface, index: number) => (
-                    <li key={location.name + '-' + index}>
-                        <button
-                            className='item'
-                            onClick={() => onLocationSelect(location)}
-                        >
-                            {location.name}, {location.country}
-                        </button>
-                    </li>
-                ))}
-            </ul>
-            <button
-                className='button'
-                onClick={onSearch}
-            >
-                Add
-            </button>
+        <div className='center'>
+            <div className='container'>
+                <div className='input-container'>
+                    <input
+                        className='input'
+                        type='text'
+                        value={city}
+                        onChange={onInputChange}
+                    />
+                    <ul className='list'>
+                        {locations.map((location: LocationInterface, index: number) => (
+                            <li key={location.name + '-' + index}>
+                                <button
+                                    className='item'
+                                    onClick={() => onLocationSelect(location)}
+                                >
+                                    {location.name}, {location.country}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <button className='button' onClick={onSearch}>
+                    Add
+                </button>
+            </div>
         </div>
     );
 };
