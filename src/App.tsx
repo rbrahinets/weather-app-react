4315fb48129ba006e8729bcff6c630082f58c4fd
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-13 18:40:14 +0300

    Add function for handling input change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
 const App: React.FC = () => {
+    const [city, setCity] = useState<string>('');
+
+    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value.trim();
+        setCity(event.target.value);
+
+        if (value !== '') {
+            console.log(value);
+        }
+    };
+
     return (
         <>
             <Header/>
