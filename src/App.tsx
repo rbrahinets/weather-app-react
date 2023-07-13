4e6f66b6fdcd8d1be7daf6c9399bad1a932d4ee3
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-13 20:16:38 +0300

    Center the forecast

--- src/App.tsx
+++ src/App.tsx
@@ -81,9 +81,11 @@ const App: React.FC = () => {
             />
 
             {forecast && (
-                <div>
-                    <h1>{forecast.name}, {forecast.country}</h1>
-                    <h1>{Math.round(forecast.list[0].main.temp - 273.15)}<sup>o</sup></h1>
+                <div className='center'>
+                    <div>
+                        <h1>{forecast.name}, {forecast.country}</h1>
+                        <h1>{Math.round(forecast.list[0].main.temp - 273.15)}<sup>o</sup></h1>
+                    </div>
                 </div>
             )}
         </>
