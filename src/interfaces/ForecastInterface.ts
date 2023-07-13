6044c32aaae8559f8b297f1f947f34884a2e0566
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-13 19:00:15 +0300

    Create interface for forecast

---
+++ src/interfaces/ForecastInterface.ts
@@ -0,0 +1,36 @@
+export interface ForecastInterface {
+    name: string;
+    country: string;
+    list: [
+        {
+            dt: number;
+            main: {
+                feels_like: number;
+                humidity: number;
+                pressure: number;
+                temp: number;
+                temp_max: number;
+                temp_min: number;
+            };
+            weather: [
+                {
+                    main: string;
+                    icon: string;
+                    description: string;
+                }
+            ];
+            wind: {
+                speed: number;
+                gust: number;
+                deg: number;
+            };
+            clouds: {
+                all: number;
+            };
+            pop: number;
+            visibility: number;
+        }
+    ];
+    sunrise: number;
+    sunset: number;
+}
