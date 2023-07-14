01d6aeb44db44043e0074d7ff6174030abacf152
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-14 15:54:35 +0300

    Add interface for location state

--- src/interfaces/StatesInterface.ts
+++ src/interfaces/StatesInterface.ts
@@ -1,3 +1,13 @@
+import {LocationInterface} from './LocationInterface';
+
 export interface InputStateInterface {
     input: string,
+}
+
+export interface LocationsStateInterface {
+    locations: LocationInterface[],
+}
+
+export interface LocationStateInterface {
+    location: LocationInterface | null,
 }
