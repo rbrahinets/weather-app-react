8610beee13a09e7a9fe7f4727125ef2fcfccdb9c
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-14 23:24:24 +0300

    Display date and time of forecast

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -7,11 +7,44 @@ const Forecast: React.FC<ForecastPropsInterface> = (
         forecast
     }
 ) => {
+    const today = forecast.list[0];
+    const date: Date = new Date(today.dt * 1000);
+    const weekdays: string[] = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+    const months: string[] = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+    const weekday: string = weekdays[date.getUTCDay()];
+    const day: number = date.getUTCDate();
+    const month: string = months[date.getUTCMonth()];
+    const hours: string = String(date.getUTCHours()).padStart(2, '0');
+    const minutes: string = String(date.getUTCMinutes()).padStart(2, '0');
+    const formattedDateTime: string = `${weekday}, ${day} ${month}, ${hours}:${minutes}`;
+
     return (
         <div className='forecast-container'>
             <div className='section section-top'>
                 <div className='section-top-left'>
                     <div className='bold'>{forecast.name}, {forecast.country}</div>
+                    <div>{formattedDateTime}</div>
                 </div>
                 <div className='section-top-right'>
                 </div>
