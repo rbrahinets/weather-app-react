812009fdf74254e0e57ca78af3ad073bb6238463
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-14 15:38:00 +0300

    Add action for setting locations

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,12 +1,22 @@
 import {Dispatch} from 'redux';
 import {ActionType} from '../action-types';
 import {Action} from '../actions';
+import {LocationInterface} from '../../interfaces/LocationInterface';
 
 export const setInput = (input: string) => {
     return (dispatch: Dispatch<Action>) => {
         dispatch({
             type: ActionType.SET_INPUT,
             payload: input,
+        });
+    };
+};
+
+export const setLocations = (locations: LocationInterface[]) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.SET_LOCATIONS,
+            payload: locations,
         });
     };
 };
