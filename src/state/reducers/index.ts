8ea601aa53ef6377c04aa1b151e24e1a9a141bf6
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-15 16:20:01 +0300

    Add reducer for isCelsius to state

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -3,12 +3,14 @@ import inputReducer from './inputReducer';
 import locationReducer from './locationReducer';
 import locationsReducer from './locationsReducer';
 import forecastReducer from './forecastReducer';
+import isCelsiusReducer from './isCelsiusReducer';
 
 const reducers = combineReducers({
     input: inputReducer,
     locations: locationsReducer,
     location: locationReducer,
     forecast: forecastReducer,
+    isCelsius: isCelsiusReducer,
 });
 
 export default reducers;
