ec5761d336789ce51a460689279d328092940307
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-16 21:05:41 +0300

    Add action type for updating forecast

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -3,6 +3,7 @@ export enum ActionType {
     SET_LOCATIONS = 'setLocations',
     SET_LOCATION = 'setLocation',
     SET_FORECAST = 'setForecast',
+    UPDATE_FORECAST = 'updateForecast',
     DELETE_FORECAST = 'deleteForecast',
     SET_IS_CELSIUS = 'setIsCelsius',
 }
